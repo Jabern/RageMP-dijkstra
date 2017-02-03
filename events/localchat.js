2ef1bd58ea389b0c371eef202ee0e0410a9219bf
Prakkkmak <Prakkmak@gmail.com>
2017-02-03 10:00:22 +0100

    Correction
    C'est bon dans l'ensemble

--- events/localchat.js
+++ events/localchat.js
@@ -1,17 +1,17 @@
 "use strict"; // on utilise le mode strict
 
-module.exports = class // Exporter un module qui contient une class
+class Vector3 // Exporter un module qui contient une class
 {
 
   constructor(x, y, z, radius) // Voir : https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Classes/constructor
   {
     this.x = x;
     this.y = y;
-    this.z = z;
-    this.radius = radius || 2;
+    this.z = z; 
+    // this.radius = radius || 2; Ca veut rien dire "radius ou 2"
   }
 
-  static GetVectorDistance(v1 , v2) // Création de la fonction qui compare deux Vector3
+  static getVectorDistance(v1 , v2) // Création de la fonction qui compare deux Vector3
   {
     var vx = v1.x - v2.x; // on initialise le Vector1 qui Vaut a X
     var vy = v1.y - v2.y; // on initialise le Vector2 qui Vaut a Y
@@ -20,11 +20,11 @@ module.exports = class // Exporter un module qui contient une class
     return (vx * vx + vy * vy + vz * vz); // On retourne la racine de Delta vx + Delta vy + Delta vz
   }
 
-  static SendNearbyMessage(player, radius, str)
+  static sendNearbyMessage(player, radius, str)
   {
     radius *= radius;
     mp.players.forEach( (_player) => {
-      if(module.exports.GetVectorDistance(player.position, _player.position) <= radius)
+      if(Vector3.GetVectorDistance(player.position, _player.position) <= radius)
       {
         player.outputChatBox(str);
       }
@@ -35,10 +35,11 @@ module.exports = class // Exporter un module qui contient une class
 function OnPlayerChat(player, text)
 {
   let string = "<b><FONT COLOR=#ff0000 >["+ player.name +"] : </FONT></b>"+ text
-  module.exports.SendNearbyMessage(player, 2, string);
+  Vector3.SendNearbyMessage(player, 2, string);
 }
 
 mp.events.add(
 {
     "playerChat" : OnPlayerChat
 });
+module.exports = Vector3
